02ffdf46947cc1cd0e54ac85b062f05c61eb05b0
malilasage <malilaclearwater@gmail.com>
2016-11-09 14:30:51 -0700

    express server can send different responses

--- expressServer.js
+++ expressServer.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8000;
 var fs = require('fs');
 
 
-app.use(function(req, res) {
+app.get('/pets', function(req, res) {
   fs.readFile('./pets.json', 'utf8', function(err, data) {
     if(err) {
       throw err;
@@ -14,6 +14,10 @@ app.use(function(req, res) {
     var pets = JSON.parse(data);
     res.send(pets);
   });
+})
+
+app.use(function(req, res) {
+  res.sendStatus(404);
 });
 
 app.listen(port, function() {
