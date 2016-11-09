f1125700da5a9005e905f691d368d81d70660360
malilasage <malilaclearwater@gmail.com>
2016-11-09 14:50:43 -0700

    showing pets at index 0, not index 1

--- expressServer.js
+++ expressServer.js
@@ -1,18 +1,41 @@
 'use strict';
+//why aren't these happening in the same space as pets.js js its because nodemon is only running expressServer.js
 
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+
 var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
 
 
 app.get('/pets', function(req, res) {
-  fs.readFile('./pets.json', 'utf8', function(err, data) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
     if(err) {
-      throw err;
+      console.err(err.stack);
+      return res.sendStatus(500);
     }
-    var pets = JSON.parse(data);
+    var pets = JSON.parse(petsJSON);
     res.send(pets);
+  });
+});
+
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if(err) {
+      console.err(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+    res.set('Content-Type', 'text/plain');
+    res.send(pets[id]);
   });
 })
 
