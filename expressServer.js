cb1328a05127a3a28cfc6e0c3968e68059dfd908
malilasage <malilaclearwater@gmail.com>
2016-11-09 14:26:57 -0700

    server request returns pets

--- expressServer.js
+++ expressServer.js
@@ -3,11 +3,21 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+
 
 app.use(function(req, res) {
-  res.send('Hello World');
+  fs.readFile('./pets.json', 'utf8', function(err, data) {
+    if(err) {
+      throw err;
+    }
+    var pets = JSON.parse(data);
+    res.send(pets);
+  });
 });
 
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
+
+module.exports = app;
